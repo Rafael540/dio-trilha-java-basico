e534007be8123fd9762cba3b14727b1f44e4d144
Rafael540 <alves540@hotmail.com>
2024-05-01 15:18:58 -0300

    Mudanças Tipos/Variaveis

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,19 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       double salarioMinino = 2.500;
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+
+       int numero = 5;
+       
+       numero = 10;
+
+       System.out.print(numero);
+
+       final double Valor_PI = 3.14;
+
+
     }
 }
